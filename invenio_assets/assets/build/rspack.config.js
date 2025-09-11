6642e35e7a421d6f581f14450bbe3f95d0d0997c
Martin Fenner <martin@front-matter.io>
2025-09-11 20:29:22 +0200

    initial version

--- invenio_assets/assets/build/rspack.config.js
+++ invenio_assets/assets/build/rspack.config.js
@@ -23,7 +23,7 @@ if (config.aliases) {
     Object.entries(config.aliases).map(([alias, alias_path]) => [
       alias,
       path.resolve(config.build.context, alias_path),
-    ]),
+    ])
   );
 }
 
@@ -119,6 +119,11 @@ var webpackConfig = {
   },
   module: {
     rules: [
+      {
+        test: /\.css$/,
+        use: ["postcss-loader"],
+        type: "css",
+      },
       {
         test: require.resolve("jquery"),
         use: [
@@ -232,7 +237,7 @@ var webpackConfig = {
   performance: { hints: false },
   watchOptions: {
     followSymlinks: true,
-    ignored: '**/.git', // The default would also ignore "node_modules" which we don't want
+    ignored: "**/.git", // The default would also ignore "node_modules" which we don't want
   },
   experiments: {
     css: false,
